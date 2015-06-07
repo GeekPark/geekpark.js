6256d077e007c09ad3fddc26b65885e2c8b5699a
Vanilla <mail@liyaodong.com>
2015-06-07 16:20:16 +0800

    增加利用UA检测是否未移动设备的接口

--- src/geekpark.js
+++ src/geekpark.js
@@ -166,6 +166,11 @@ $(function() {
     // 检测页面是否有JS权限
     isPage: function(pageClass) {
       return $('body').hasClass(pageClass) ? true : false;
+    },
+
+    // 利用UA检测是否为移动设备
+    isMobile: function () {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? true : false;
     }
 
   }; // GeekPark object define end
